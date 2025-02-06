5ec29660086ea4c5256b1d92d724ba915dd51ee8
Brayden <foersteb@oregonstate.edu>
2025-02-05 17:37:39 -0700

    Add without_duplicates.py

---
+++ without_duplicates.py
@@ -0,0 +1,12 @@
+def without_duplicates(lst):
+    unique_list = []
+    for item in lst:
+        if item not in unique_list:
+            unique_list.append(item)
+    return unique_list
+
+# Example usage
+if __name__ == "__main__":
+    my_list = [8, 'hello', 8, True, -1000000.4, 'hello', 8]
+    result = without_duplicates(my_list)
+    print(result)
