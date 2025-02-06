209df2a3bbfb5c5031624cb05a60c48ef22ed91b
braydenfoerster <106361516+braydenfoerster@users.noreply.github.com>
2025-02-05 17:39:07 -0700

    without_duplicates.py

--- without_duplicates.py
+++ without_duplicates.py
@@ -5,7 +5,7 @@ def without_duplicates(lst):
             unique_list.append(item)
     return unique_list
 
-# Example usage
+# Example
 if __name__ == "__main__":
     my_list = [8, 'hello', 8, True, -1000000.4, 'hello', 8]
     result = without_duplicates(my_list)
